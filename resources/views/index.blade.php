95e4159689f4a336e7746bb9541b7375cc18551f
humayonkabirr <humayonkobir8@gmail.com>
2024-05-20 18:01:05 +0600

    hero section bg image center

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,7 +13,7 @@
 <body class="bg-slate-950">
 
     <!-- nav bar -->
-    <nav class="sticky top-0 py-4 text-gray-300 shadow-md shadow-slate-950 backdrop-blur-md bg-slate-950/30">
+    <nav class="absolute sticky top-0 py-4 text-gray-300 shadow-md shadow-slate-950 backdrop-blur-md bg-slate-950/30">
         <div class="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="flex items-center justify-between space-x-4 text-lg font-bold">
                 <div>
@@ -34,9 +34,9 @@
     <!-- nav bar end -->
 
 
-    <section class="w-full bg-left bg-cover bg-[url('/public/assets/images/glow-bottom.svg')]">
+    <section class="w-full bg-center bg-[url('/public/assets/images/glow-bottom.svg')]">
         <div class="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="flex items-center justify-center w-full py-24 space-x-2 shadow-lg">
+            <div class="flex items-center justify-center w-full py-24 space-x-2">
                 <div class="text-[40px] text-gray-300">
                     <h1>Welcome </h1>
                     <h1 class="text-[25px]">to kabir dev</h1>
@@ -44,7 +44,8 @@
                     <div class="text-lg">
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi sapiente reprehenderit
                             repellat possimus veniam non recusandae eius dignissimos impedit. Officia in placeat
-                            accusamus consequatur qui odio a dolorum excepturi autem?</p>
+                            accusamus consequatur qui odio a dolorum excepturi autem?
+                        </p>
                     </div>
                 </div>
                 <div class="w-full text-[40px] text-gray-300 text-right">
@@ -90,7 +91,7 @@
                     <h1 class="text-[40px] text-gray-300 font-bold">Protfolio</h1>
                 </div>
 
-                <div class="grid grid-cols-4 gap-4">
+                <div class="grid grid-cols-2 gap-2 md:gap-4 md:grid-cols-4">
                     <div class="p-2 border">
                         <img src="https://picsum.photos/seed/picsum/800/800" alt="">
 
